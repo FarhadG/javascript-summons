5059dd148464ce204db0e83cf093c00dbe1df63f
Farhad Ghayour <me@farhadg.com>
2013-12-08 17:48:51 +0800

    cleaned up spec

--- spec/transposeSpec.js
+++ spec/transposeSpec.js
@@ -3,10 +3,20 @@ var expect = require("../vendor/expect");
 
 describe("Transpose", function() {
   it("convert between row-oriented and column-oriented representations", function() {
-    expect(transpose([[0, 1, 2], [3, 4, 5], [6, 7, 8]])).to.eql([[0, 3, 6], [1, 4, 7], [2, 5, 8]]);
+    var input  = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    var result = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
+    expect(transpose(input)).to.eql(result);
   })
 
   it("convert between asymmetrical representations", function() {
-    expect(transpose([[0, 1, 2, 3], [4, 5], [6, 7, 8, 9]])).to.eql([[0, 4, 6], [1, 5, 7], [2, undefined, 8], [3, undefined, 9]]);
+    var input  = [[0, 1, 2, 3],
+                  [4, 5], 
+                  [6, 7, 8, 9]];
+                  
+    var result = [[0, 4, 6], 
+                  [1, 5, 7], 
+                  [2, undefined, 8], 
+                  [3, undefined, 9]];
+    expect(transpose(input)).to.eql(result);
   })
 })
