093802273bcb11aedab0c939cfe2b9148c062895
Farhad Ghayour <me@farhadg.com>
2013-12-08 17:38:16 +0800

    summoned inWords

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ Do you have what it takes to summon Javascript? Let's find out!
 
 <tr>
 <td>Exercises</td>
-<td>9</td>
+<td>10</td>
 </tr>
 </table>
 
---
+++ src/inWords.js
@@ -0,0 +1,15 @@
+/* ############### IN WORDS
+
+Write a function that takes a single positive integer 
+and returns its equivalent in words.
+
+For example: inWords(27) === "twenty seven"
+
+The function should work for all numbers ranging
+from zero to the trillions.
+
+################## */
+
+inWords = function(num) {
+  // code your magic here
+};
