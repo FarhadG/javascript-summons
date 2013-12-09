6bbcfe760023c2bca47bfb381c790f29206509a6
Farhad Ghayour <me@farhadg.com>
2013-12-09 16:47:25 +0800

    summoned orederedVowels

---
+++ src/22_orderedVowels.js
@@ -0,0 +1,13 @@
+/* ############### ORDERED VOWELS
+
+  Write a functions that takes a string of lowercase words and 
+  returns a string with just the words containing
+  all their vowels in alphabetical order.
+
+  For example: orderedVowels("apple") === "apple"
+
+################## */
+
+orderedVowels = function(str) {
+  // code your magic here
+};
