2dad8cd788720c810619744ac528655c0913e36e
Farhad Ghayour <me@farhadg.com>
2013-12-09 17:55:20 +0800

    cleaned up spec

--- spec/04_medianSpec.js
+++ spec/04_medianSpec.js
@@ -6,11 +6,11 @@ describe("Median", function() {
     expect(median([])).to.eql([]);
   })
 
-  it("returns the median for an array containing an odd number of integers", function() {
-    expect(median([1, 3, 5, 2, 9])).to.eql(3);
-  })
-
   it("returns the median for an array containing an even number of integers", function() {
     expect(median([1, 3, 5, 6, 2, 9])).to.eql(4);
+  })
+
+  it("returns the median for an array containing an odd number of integers", function() {
+    expect(median([1, 3, 5, 2, 9])).to.eql(3);
   })
 })
